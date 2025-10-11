1455ee7ffbdd2e443ea6bcef788e6c290d199436
iridiaaaa <79019@alumnos.utleon.edu.mx>
2025-10-11 10:10:01 -0600

    rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 //import { DistanciaComponent } from "./formularios/distancia/distancia.component";
 //import { MultiplicacionComponent } from "./formularios/multiplicacion/multiplicacion.component";
-import { ZodiacoComponent } from "./formularios/zodiaco/zodiaco.component";
+//import { ZodiacoComponent } from "./formularios/zodiaco/zodiaco.component";
+import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
     //DistanciaComponent, 
     //MultiplicacionComponent, 
-    ZodiacoComponent],
+    //ZodiacoComponent, 
+    NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
   title = 'AngularSegundo02';
+  ngOnInit():void{
+    initFlowbite();
+  }
 }
